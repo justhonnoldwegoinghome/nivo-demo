37aa9b6350de695cce8221b22bf2e8834e0ec984
justhonnoldwegoinghome <wesleytan19@gmail.com>
2022-10-27 16:16:08 +0800

    change color of dots

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ export function App() {
           type: "time",
           format: "%Y-%m-%d",
         }}
+        colors={{ scheme: "category10" }}
         xFormat="time:%Y-%m-%d"
         yScale={{
           type: "linear",
